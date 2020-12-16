a6ae21192ca0a6192a0c90d92b0d620518c210a4
danilocorcisp <danilocorci@gmail.com>
2020-12-16 17:27:53 +0100

    preparing for data receiving

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,19 +1,17 @@
 import React from "react";
 
-const Episode = () => {
+const Episode = ({ title, image, trackUrl }) => {
     return (
         <div className="d-block d-md-flex podcast-entry bg-white mb-5">
             <div
                 className="image"
                 style={{
-                    backgroundImage: "url('images/img_1.jpg')",
+                    backgroundImage: `url('${image}')`,
                 }}
             ></div>
             <div className="text">
                 <h3 className="font-weight-light">
-                    <a href="single-post.html">
-                        Episode 08: How To Create Web Page Using Bootstrap 4
-                    </a>
+                    <a href="single-post.html">{title}</a>
                 </h3>
                 <div className="text-white mb-3">
                     <span className="text-black-opacity-05">
@@ -34,10 +32,7 @@ const Episode = () => {
                         controlsList="nodownload"
                         style={{ width: "100%" }}
                     >
-                        <source
-                            src="http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88"
-                            type="audio/mp3"
-                        />
+                        <source src={trackUrl} type="audio/mp3" />
                     </audio>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,63 @@ import PodcastRow from "./components/PodcastRow";
 import Episode from "./components/Episode";
 
 const App = () => {
+    const podcasts = [
+        {
+            id: 0,
+            name: "pod1",
+            image: "/images/person_1.jpg",
+            categories: ["sports", "culture"],
+        },
+        {
+            id: 1,
+            name: "pod2",
+            image: "/images/person_2.jpg",
+            categories: ["history", "jazz"],
+        },
+        {
+            id: 2,
+            name: "pod3",
+            image: "/images/person_3.jpg",
+            categories: ["horror", "scifi"],
+        },
+    ];
+    const episodes = [
+        {
+            id: 0,
+            title: "Episode 1",
+            image: "/images/img_1.jpg",
+            trackUrl:
+                "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
+        },
+        {
+            id: 1,
+            title: "Episode 2",
+            image: "/images/img_2.jpg",
+            trackUrl:
+                "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
+        },
+        {
+            id: 2,
+            title: "Episode 3",
+            image: "/images/img_3.jpg",
+            trackUrl:
+                "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
+        },
+        {
+            id: 3,
+            title: "Episode 4",
+            image: "/images/img_4.jpg",
+            trackUrl:
+                "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
+        },
+        {
+            id: 4,
+            title: "Episode 5",
+            image: "/images/img_5.jpg",
+            trackUrl:
+                "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
+        },
+    ];
     return (
         <div className="site-wrap">
             <Header />
@@ -28,17 +85,19 @@ const App = () => {
                                     </button>
                                 </div>
                                 <ul className="list-unstyled">
-                                    <PodcastRow />
-                                    <PodcastRow />
-                                    <PodcastRow />
-                                    <PodcastRow />
+                                    {podcasts.map((podcast) => (
+                                        <PodcastRow
+                                            key={podcast.id}
+                                            {...podcast}
+                                        />
+                                    ))}
                                 </ul>
                             </div>
                         </div>
                         <div className="col-lg-9">
-                            <Episode />
-                            <Episode />
-                            <Episode />
+                            {episodes.map((episode) => (
+                                <Episode key={episode.id} {...episode} />
+                            ))}
                         </div>
                     </div>
                 </div>
