634f83f274e15b377b8425706ae1f10afe49236a
danilocorcisp <danilocorci@gmail.com>
2020-12-17 16:52:06 +0100

    search connection to live data

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,60 @@
 const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
     res.render("index");
 });
 
-// router.get("/react", (req, res, next) => {
-//     res.render("index");
-// });
+router.get("/search", async (req, res, next) => {
+    const url = "https://itunes.apple.com/search?term=sports&media=podcast";
+    const { data } = await axios({
+        url,
+        method: "get",
+        options: {
+            headers: { Accept: "application/json" },
+        },
+    });
+    const { results } = data;
+    const podcasts = results.map((podcast) => {
+        return {
+            id: podcast.artistId,
+            name: podcast.artistName,
+            trackName: podcast.trackName,
+            image: podcast.artworkUrl600,
+            categories: podcast.genres,
+            feed: podcast.feedUrl,
+        };
+    });
+    res.json({
+        results,
+    });
+});
+
+router.post("/search", async (req, res, next) => {
+    const searchTerm = req.body.term;
+    const url = `https://itunes.apple.com/search?term=${searchTerm}&media=podcast`;
+    const { data } = await axios({
+        url,
+        method: "get",
+        options: {
+            headers: { Accept: "application/json" },
+        },
+    });
+    const { results } = data;
+    const podcasts = results.map((podcast) => {
+        return {
+            id: podcast.artistId,
+            name: podcast.artistName,
+            trackName: podcast.trackName,
+            image: podcast.artworkUrl600,
+            categories: podcast.genres,
+            feed: podcast.feedUrl,
+        };
+    });
+    res.json({
+        podcasts,
+    });
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 import Header from "./components/Header";
 import PodcastRow from "./components/PodcastRow";
 import Episode from "./components/Episode";
+import axios from "axios";
 
 const App = () => {
-    const podcasts = [
-        {
-            id: 0,
-            name: "pod1",
-            image: "/images/person_1.jpg",
-            categories: ["sports", "culture"],
-        },
-        {
-            id: 1,
-            name: "pod2",
-            image: "/images/person_2.jpg",
-            categories: ["history", "jazz"],
-        },
-        {
-            id: 2,
-            name: "pod3",
-            image: "/images/person_3.jpg",
-            categories: ["horror", "scifi"],
-        },
-    ];
+    const [searchTerm, setSearchTerm] = useState("");
+    const [podcasts, setPodcasts] = useState([]);
+    // const podcasts = [
+    //     {
+    //         id: 0,
+    //         name: "pod1",
+    //         image: "/images/person_1.jpg",
+    //         categories: ["sports", "culture"],
+    //     },
+    //     {
+    //         id: 1,
+    //         name: "pod2",
+    //         image: "/images/person_2.jpg",
+    //         categories: ["history", "jazz"],
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "pod3",
+    //         image: "/images/person_3.jpg",
+    //         categories: ["horror", "scifi"],
+    //     },
+    // ];
     const episodes = [
         {
             id: 0,
@@ -62,6 +65,35 @@ const App = () => {
                 "http://hwcdn.libsyn.com/p/e/2/d/e2d49676d65218ec/p1541a.mp3?c_id=84308228&cs_id=84308228&expiration=1601254668&hwt=ccab3206052417d0e901722ab00c9c88",
         },
     ];
+
+    const onInputTyped = (e) => {
+        console.log(
+            "🚀 ~ file: index.js ~ line 70 ~ onInputTyped ~ e",
+            e.target.value
+        );
+        setSearchTerm(e.target.value);
+    };
+
+    const onSearchBtnClicked = (e) => {
+        e.preventDefault();
+        console.log("🚀 ~ file: index.js ~ line 76 ~ App ~ e", searchTerm);
+        axios({
+            url: "/search",
+            method: "post",
+            data: {
+                term: searchTerm.trim().toLocaleLowerCase(),
+            },
+            options: {
+                headers: { Accept: "application/json" },
+            },
+        })
+            .then(({ data }) => {
+                console.log("PODCASTS: " + JSON.stringify(data));
+                setPodcasts(data.podcasts);
+            })
+            .catch((err) => {});
+    };
+
     return (
         <div className="site-wrap">
             <Header />
@@ -73,11 +105,13 @@ const App = () => {
                                 <h3 className="mb-2">Search Podcasts</h3>
                                 <div style={{ display: "flex" }}>
                                     <input
+                                        onChange={onInputTyped}
                                         type="text"
                                         style={{ height: 32 }}
                                         className="form-control mb-4"
                                     />
                                     <button
+                                        onClick={onSearchBtnClicked}
                                         className="btn btn-info p-1 ml-2"
                                         style={{ height: 32 }}
                                     >
